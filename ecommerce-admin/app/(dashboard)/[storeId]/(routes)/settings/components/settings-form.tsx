984a00f0eedb1c31c91f0210d6942c9a8b24a3f4
soeviturek <wuyc.james@gmail.com>
2024-04-26 02:05:36 +1000

    migrated to cockroachdb, updated settings form

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -4,19 +4,50 @@ import { Store } from "@prisma/client";
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
-
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Form } from "@/components/ui/form";
 interface SettingsFormProps{
     initialData: Store;
 };
+const formSchema = z.object({
+    name:z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
 
 // this component is only used in settings therefore the folder can be put under settings
 export const SettingsForm:React.FC<SettingsFormProps> = ({initialData}) => {
+
+    //controlls alert model, slightly different from store modal
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+
+    const onSubmit = async (data: SettingsFormValues) =>{
+        console.log(data);
+    };
+    
     return (
-        <div className="flex items-center justify-between">
-            <Heading title="Settings" description="Manage store preferences" />
-            <Button variant="destructive" size="sm" onClick={()=>{}}>
-                <Trash className="h-4 w-4" />
-            </Button>
-        </div>
+        <>
+            <div className="flex items-center justify-between">
+                <Heading title="Settings" description="Manage store preferences" />
+                <Button variant="destructive" size="sm" onClick={()=>{}}>
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+            <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                </form>
+            </Form>
+        </>
     )
 };
---
+++ ecommerce-admin/prisma/migrations/20240423062102_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
+);
